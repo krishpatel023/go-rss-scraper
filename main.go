306a99af3b53177a9aa7d04230eb5efa5588eadf
krishpatel023 <krishpatel0234@gmail.com>
2025-02-05 20:54:31 +0530

    add: godotenv

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main(){
-	fmt.Println("hello world")
+	// load .env file and get PORT
+	godotenv.Load(".env")
+	portString := os.Getenv("PORT")
+
+	if portString == "" {
+		log.Fatal("$PORT must be set in env")
+	}
+	fmt.Println("PORT: " + portString)
 }
