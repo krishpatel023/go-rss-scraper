c7f853e5b1eb3dc48dc3cc4199922bf1c061fe61
krishpatel023 <krishpatel0234@gmail.com>
2025-02-09 21:38:02 +0530

    fix: added strict schema verification while parsing

--- backend/utils/params.go
+++ backend/utils/params.go
@@ -11,6 +11,9 @@ func GetParams[T any](r *http.Request, w http.ResponseWriter) *T {
 	decoder := json.NewDecoder(r.Body)
 	params := new(T)
 
+	// Enforce strict JSON decoding (no extra fields allowed)
+	decoder.DisallowUnknownFields()
+
 	err := decoder.Decode(params)
 	if err != nil {
 		RespondWithError(w, http.StatusBadRequest, fmt.Sprintf("Error parsing JSON: %v", err))
